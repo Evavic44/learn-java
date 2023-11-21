05206627dbb72940a349e2e151ae55e860fa710b
Victor Eke <evavic44@gmail.com>
2023-11-21 10:24:52 +0100

    initial commit

---
+++ src/Scanner.java
@@ -0,0 +1,12 @@
+
+
+public class Scanner {
+	public static void main(String[] args) {
+		
+	}
+}
+
+/*
+Scanner is a class that is used for getting input from a user. It is defined in the package
+ 
+*/
